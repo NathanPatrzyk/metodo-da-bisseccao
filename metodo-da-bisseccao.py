d56e528d56e09530bfc8bbf6e1f8fff4a0be021e
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-02-24 19:25:08 -0300

    Finalizando método da bisseccao otimizado.

--- metodo-da-bisseccao.py
+++ metodo-da-bisseccao.py
@@ -2,23 +2,30 @@ from funcao import Funcao
 
 
 def calcularBisseccao(f, a, b, n):
-    m = (a + b) / 2.0
 
-    if n == 0:
-        return m
-    if (f.calcularValor(a)*f.calcularValor(m)) < 0:
-        return calcularBisseccao(f, a, m, n-1)
-    else:
-        return calcularBisseccao(f, m, b, n-1)
+    fa = f.calcularValor(a)
+    fb = f.calcularValor(b)
+
+    while n > 0:
+
+        m = (a + b) / 2.0
+
+        fm = f.calcularValor(m)
+
+        if fa*fm < 0:
+            b = m
+            fb = fm
+        else:
+            a = m
+            fa = fm
+
+        n = n-1
+
+    return m
 
 
 f = Funcao(1, 0, -4, 8)
 print(str(f.a) + "x^3 + " + str(f.b) + "x^2 + " + str(f.c) + "x + " + str(f.d) + " = 0")
-print()
-
-solucao = calcularBisseccao(f, -50, 50, 0)
-print("Solução encontrada com 0 iterações: x = " + str(solucao))
-print("Teste da solução encontrada com 0 iterações: f(" + str(solucao) + ") = " + str(f.calcularValor(solucao)))
 print()
 
 solucao = calcularBisseccao(f, -50, 50, 1)
